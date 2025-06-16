82830a864e92de8b9b85663d34bd73fd57778ee6
engisalor <50170623+engisalor@users.noreply.github.com>
2025-06-16 08:58:14 +0200

    test

---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import ast
+import hashlib
+import typing
+from ast import literal_eval
+from pathlib import Path
+
+y = literal_eval("[1]")
+
+i = Path("heelo")
+
+a = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"
+
+b = f"{i}"
+
+n = "here"
+
+
+def func():
+    """
+
+    Not a good"""
+
+
+b = typing.List
+
+{"1": 1}.get(2, None)
+
+k = ast.literal_eval("1")
