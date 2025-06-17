d9a5a6b75484033dcd0f5b0ae45d73d1f4d66a63
engisalor <50170623+engisalor@users.noreply.github.com>
2025-06-17 19:13:19 +0200

    test: add basic tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,68 @@
-import ast
-import hashlib
-import typing
-from ast import literal_eval
 from pathlib import Path
 
-y = literal_eval("[1]")
+from click.testing import CliRunner
 
-i = Path("heelo")
-
-a = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"
-
-b = f"{i}"
-
-n = "here"
+from clilm.main import cli
 
 
-def func():
-    """
+def test_cli_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--help"])
+    assert result.stdout.startswith("Usage:")
 
-    Not a good"""
+
+def test_prepare_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["prepare", "--help"])
+    assert result.stdout.startswith("Usage:")
 
 
-b = typing.List
+def test_query_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["query", "--help"])
+    assert result.stdout.startswith("Usage:")
 
-{"1": 1}.get(2, None)
 
-k = ast.literal_eval("1")
+def test_prepare():
+    runner = CliRunner()
+
+    ls = [
+        "--clear",
+        "-p",
+        "wizard-of-math",
+        "prepare",
+    ]
+    result = runner.invoke(cli, ls)
+    assert result.exit_code == 0
+    assert Path("projects/wizard-of-math/prompt/run.1.yml").exists()
+
+
+def test_query():
+    runner = CliRunner()
+    ls = [
+        "--clear",
+        "-p",
+        "wizard-of-math",
+        "prepare",
+        "query",
+    ]
+    result = runner.invoke(cli, ls)
+    assert result.exit_code == 0
+    assert Path("projects/wizard-of-math/output/run.1.yml").exists()
+    assert Path("projects/wizard-of-math/prompt/run.1.yml").exists()
+
+
+def test_empty():
+    runner = CliRunner()
+    ls = [
+        "-p",
+        "wizard-of-math",
+        "prepare",
+        "query",
+    ]
+    result = runner.invoke(cli, ls)
+    result = runner.invoke(cli, ["empty", "wizard-of-math"])
+    assert result.exit_code == 0
+    assert not Path("projects/wizard-of-math/log").exists()
+    assert not Path("projects/wizard-of-math/output").exists()
+    assert not Path("projects/wizard-of-math/prompt").exists()
